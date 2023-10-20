7df6b0b321be4772b82e8fdc1f1ca8fce38ecd8f
AR-Fahad <ashfaqurrahmanfahad@gmail.com>
2023-10-21 01:47:22 +0600

    products get & post added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
@@ -26,83 +26,19 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
 
-    const database = client.db("brandServer");
-    const samsung = database.collection("samsung");
-    const sony = database.collection("sony");
-    const apple = database.collection("apple");
-    const oneplus = database.collection("oneplus");
-    const nokia = database.collection("nokia");
-    const nothing = database.collection("nothing");
+    const database = client.db("brandsDB");
+    const products = database.collection("products");
+    // const cart = database.collection("cart");
 
-    // samsung
-    app.get("/samsung", async (req, res) => {
-      const result = await samsung.find().toArray();
+    // products
+    app.get("/products", async (req, res) => {
+      const result = await products.find().toArray();
       res.send(result);
     });
 
-    app.post("/samsung", async (req, res) => {
+    app.post("/products", async (req, res) => {
       const newItem = req.body;
-      const result = await samsung.insertOne(newItem);
-      res.send(result);
-    });
-
-    // sony
-    app.get("/sony", async (req, res) => {
-      const result = await sony.find().toArray();
-      res.send(result);
-    });
-
-    app.post("/sony", async (req, res) => {
-      const newItem = req.body;
-      const result = await sony.insertOne(newItem);
-      res.send(result);
-    });
-
-    // apple
-    app.get("/apple", async (req, res) => {
-      const result = await apple.find().toArray();
-      res.send(result);
-    });
-
-    app.post("/apple", async (req, res) => {
-      const newItem = req.body;
-      const result = await apple.insertOne(newItem);
-      res.send(result);
-    });
-
-    // onePlus
-    app.get("/oneplus", async (req, res) => {
-      const result = await oneplus.find().toArray();
-      res.send(result);
-    });
-
-    app.post("/oneplus", async (req, res) => {
-      const newItem = req.body;
-      const result = await oneplus.insertOne(newItem);
-      res.send(result);
-    });
-
-    // nokia
-    app.get("/nokia", async (req, res) => {
-      const result = await nokia.find().toArray();
-      res.send(result);
-    });
-
-    app.post("/nokia", async (req, res) => {
-      const newItem = req.body;
-      const result = await nokia.insertOne(newItem);
-      res.send(result);
-    });
-
-    // nothing
-    app.get("/nothing", async (req, res) => {
-      const result = await nothing.find().toArray();
-      res.send(result);
-    });
-
-    app.post("/nothing", async (req, res) => {
-      const newItem = req.body;
-      const result = await nothing.insertOne(newItem);
+      const result = await products.insertOne(newItem);
       res.send(result);
     });
 
