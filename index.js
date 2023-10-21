11f062dfcb0e822b2f8dd43dfc52549893f2c8d2
AR-Fahad <ashfaqurrahmanfahad@gmail.com>
2023-10-21 14:11:32 +0600

    get a single product added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -39,6 +39,13 @@ async function run() {
     app.post("/products", async (req, res) => {
       const newItem = req.body;
       const result = await products.insertOne(newItem);
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await products.findOne(query);
       res.send(result);
     });
 
