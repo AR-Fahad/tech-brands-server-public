302f0febf6d155a62d5fcb56b08f3cd3b263ec3f
AR-Fahad <ashfaqurrahmanfahad@gmail.com>
2023-10-21 17:45:46 +0600

    delete cart product added

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cart.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
